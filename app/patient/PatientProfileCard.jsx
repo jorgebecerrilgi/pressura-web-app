1f93cbac9773a4080f4f23bd2c37b3641221858a
Jorge Becerril <110145796+jorgebecerrilgi@users.noreply.github.com>
2023-06-06 17:35:27 -0600

    PatientProfileCard now displays correct information with skeleton when loading.

--- app/patient/PatientProfileCard.jsx
+++ app/patient/PatientProfileCard.jsx
@@ -1,27 +1,58 @@
 "use client";
 
+import Skeleton from "@/components/Skeleton";
 import styles from "./PatientProfileCard.module.css";
 import Card from "@/components/Card";
 
-const PatientProfileCard = ({ name, age, height, weight, sex, date, email }) => {
+const PatientProfileCard = ({ isLoading, name, height, weight, sex, date, email }) => {
+    const birthday = date ? date.toDate().toLocaleDateString() : "Sin fecha";
+    const currentDateMiliseconds = new Date().getTime();
+    const birthdayMiliseconds = date ? date.toDate().getTime() : 0;
+    const originYear = new Date(0).getFullYear();
+    const age = date ? new Date(currentDateMiliseconds - birthdayMiliseconds).getFullYear() - originYear : "N/A";
+
+    const firstLetter = name ? name.charAt(0).toUpperCase() : ".";
+    const iconColorHue = name
+        ? name.split("").reduce((prev, letter) => {
+              return ((letter.toLowerCase().charCodeAt(0) - 97) * 8 + prev) % 255;
+          }, 0)
+        : 0;
+
     return (
         <Card className={styles.patientProfileCard}>
-            {/* <img src={profile} width="60%" /> */}
-            <h3>{name}</h3>
-            <ul>
-                <li className={styles.title}>Edad</li>
-                <li>{age}</li>
-                <li className={styles.title}>Altura</li>
-                <li>{height}</li>
-                <li className={styles.title}>Peso</li>
-                <li>{weight}</li>
-                <li className={styles.title}>Sexo</li>
-                <li>{sex}</li>
-                <li className={styles.title}>Fecha de Nacimiento</li>
-                <li>{date}</li>
-                <li className={styles.title}>Correo Electrónico</li>
-                <li>{email}</li>
-            </ul>
+            {isLoading ? (
+                <>
+                    <Skeleton height={80} width={80} marginBottom={8} />
+                    <Skeleton height={32} marginBottom={24} />
+                    <Skeleton height={42} marginBottom={8} />
+                    <Skeleton height={42} marginBottom={8} />
+                    <Skeleton height={42} marginBottom={8} />
+                    <Skeleton height={42} marginBottom={8} />
+                    <Skeleton height={42} marginBottom={8} />
+                    <Skeleton height={42} marginBottom={8} />
+                </>
+            ) : (
+                <>
+                    <div className={styles.profileIcon} style={{ backgroundColor: `hsl(${iconColorHue}, 50%, 39%)` }}>
+                        <p>{firstLetter}</p>
+                    </div>
+                    <h3>{name}</h3>
+                    <ul>
+                        <li className={styles.title}>Edad</li>
+                        <li>{age}</li>
+                        <li className={styles.title}>Altura</li>
+                        <li>{height} cm.</li>
+                        <li className={styles.title}>Peso</li>
+                        <li>{weight} kg.</li>
+                        <li className={styles.title}>Sexo</li>
+                        <li>{sex === null ? "N/A" : sex === 0 ? "Femenino" : "Masculino"}</li>
+                        <li className={styles.title}>Fecha de Nacimiento</li>
+                        <li>{birthday}</li>
+                        <li className={styles.title}>Correo Electrónico</li>
+                        <li>{email}</li>
+                    </ul>
+                </>
+            )}
         </Card>
     );
 };
--- app/patient/page.jsx
+++ app/patient/page.jsx
@@ -3,19 +3,56 @@
 import styles from "./page.module.css";
 import PatientCard from "./PatientCard";
 import PatientProfileCard from "./PatientProfileCard";
+import { useContext, useEffect, useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/src/firebase";
+import { useSearchParams } from "next/navigation";
+import { AppContext } from "../ContextProvider";
+
+const fetchInformation = async (docID) => {
+    if (docID == null) {
+        throw Error("El identificador de documento no fue pasado correctamente, y parece ser nulo.");
+    }
+    const patientDoc = doc(db, "Paciente", docID);
+    const snap = await getDoc(patientDoc);
+    if (snap.exists()) {
+        return snap.data();
+    } else {
+        throw Error(`No se encontró el documento del paciente con id: ${docID}`);
+    }
+};
 
 const Patient = () => {
+    const [information, setInformation] = useState(null);
+    const { updateData } = useContext(AppContext);
+    const params = useSearchParams();
+    const docID = params.get("id");
+
+    useEffect(() => {
+        updateData("selectedPatient", null);
+        const fetchData = async () => {
+            try {
+                const patientInformation = await fetchInformation(docID);
+                setInformation(patientInformation);
+                console.log(patientInformation);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <main className={styles.content}>
             <PatientCard />
             <PatientProfileCard
-                name={"name"}
-                age={"age"}
-                sex={"sex"}
-                height={"height"}
-                weight={"weight"}
-                date={"date"}
-                email={"email"}
+                isLoading={information === null}
+                name={information?.Nombre}
+                sex={information?.Sexo}
+                height={information?.Altura}
+                weight={information?.Peso}
+                date={information?.FechaNacimiento}
+                email={information?.CorreoElectronico}
             />
         </main>
     );
