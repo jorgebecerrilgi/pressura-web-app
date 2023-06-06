f2eaa51a327ff33db430eac4ad149a029ece2f6c
Jorge Becerril <110145796+jorgebecerrilgi@users.noreply.github.com>
2023-06-06 13:43:25 -0600

    Signing out now has a confirm modal.

--- app/SignOut.jsx
+++ app/SignOut.jsx
@@ -2,10 +2,17 @@
 
 import { signOut } from "firebase/auth";
 import { auth } from "@/src/firebase";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "./ContextProvider";
+import ModalConfirm from "@/components/ModalConfirm";
 
 const SignOut = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     const { updateData } = useContext(AppContext);
     const onSignOut = async () => {
         try {
@@ -16,7 +23,24 @@ const SignOut = () => {
         }
     };
 
-    return <button onClick={() => onSignOut()}>Cerrar Sesión</button>;
+    return (
+        <>
+            <button type="button" onClick={() => setIsModalOpen(true)}>
+                Cerrar Sesión
+            </button>
+            {isModalOpen && (
+                <ModalConfirm
+                    open
+                    title="Cerrar sesión"
+                    onClose={closeModal}
+                    onCancel={closeModal}
+                    onAccept={onSignOut}
+                >
+                    ¿Estás seguro que quieres cerrar sesión?
+                </ModalConfirm>
+            )}
+        </>
+    );
 };
 
 export default SignOut;
